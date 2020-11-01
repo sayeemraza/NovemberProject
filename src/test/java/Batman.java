315735b8d2899ce311d34c1a4260a4a6a77bbba2
sayeemraza <sayeemraza87@gmail.com>
2020-11-01 11:24:30 -0500

    batman hs cool mobile

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,2 +1,7 @@
 public class Batman {
+    public static void main(String[] args) {
+        System.out.println("I Will Catch You Joker");
+    // yes he will catch him
+        // has cool bat mobile
+    }
 }
